0ec69eef067e85cd537e3a00fd93684d4f7b91a7
Nicholas Dunn <nexela@gmail.com>
2016-07-02 16:23:19 -0400

    Adds in support for Shuttle Train, Bobs tanks and bullets.

--- settings/color-coding-sets.lua
+++
@@ -1,5 +0,0 @@
---local colors ={"red","orange","yellow","green","cyan","blue","purple","magenta","white","black"}
-
-return {
-  ["diesel-locomotive-*"] = {group="locomotives", "fuels-high"}
-}
---
+++ settings/up-items.lua
@@ -0,0 +1,10 @@
+--
+-- Disclaimer: mp warranty void if edited.
+--
+
+
+
+return {
+  ["ammo-bullets"] = {"uranium-bullet-magazine"},
+  ["ammo-shells"] = {"uranium-small-nuke-shell", "uranium-cannon-shell"}
+}
