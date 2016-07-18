b091879ffa45498c1d1f0002f545dfb27336b474
Nicholas Dunn <nexela@gmail.com>
2016-07-18 01:37:38 -0400

    Added Angelsrefining Burner Crusher

--- settings/generic-sets.lua
+++ settings/generic-sets.lua
@@ -7,5 +7,6 @@ return {
 ["unit-launcher"] = {priority=1, slots={1}, group="combatunitlauncher", "combat-units"}, --Combat Units
 ["farl"] = {group="locomotives", "fuels-high"}, --FARL
 ["shuttleTrain"] = {group="locomotives", "fuels-high"}, --ShuttleTrain
-["ammobox-gun-turret-2"]= {group="turrets", limits= {10}, "ammo-bullets" } --Ammobox
+["ammobox-gun-turret-2"]= {group="turrets", limits= {10}, "ammo-bullets" }, --Ammobox
+["burner-ore-crusher"] = {group="burners", limits={10}, "fuels-high"}
 }
